dac9242481a3494206d25114ed8ccf57558923b7
aadil10i <aadil3003@outlook.com>
2023-07-14 21:55:45 +0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image';
+import type { Metadata } from 'next';
+import Heading from '@/components/ui/Heading';
 
-interface pageProps {}
+export const metadata: Metadata = {
+  title: 'Similarity API | Home',
+  description: 'Free & open-source text similarity API',
+};
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <div className='relative h-screen flex items-center justify-center overflow-x-hidden'>
+      <div className='container pt-32 max-w-7xl mx-auto w-full h-full'>
+        <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start '>
+          <Heading
+            size='lg'
+            className='three-d text-black dark:text-light-gold'
+          ></Heading>
+        </div>
+      </div>
+    </div>
   );
 }
